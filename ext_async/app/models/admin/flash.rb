f22b8c1fbd32c38bff257662c4607c346edab332
Patrice Lebel <patleb@users.noreply.github.com>
2018-01-02 03:04:40 -0500

    better flash model

--- ext_async/app/models/admin/flash.rb
+++ ext_async/app/models/admin/flash.rb
@@ -4,10 +4,6 @@ module Admin::Flash
   included do
     rails_admin do
       list do
-        configure :id do
-          pretty_value{ value.split(':').first }
-        end
-
         configure :messages do
           pretty_value{ value.first.last }
         end
--- ext_async/db/migrate/20180102053218_create_flashes.rb
+++ ext_async/db/migrate/20180102053218_create_flashes.rb
@@ -1,10 +1,13 @@
 class CreateFlashes < ActiveRecord::Migration[5.1]
   def change
-    create_table :flashes, id: false do |t|
-      t.primary_key :id, :text
-      t.json        :messages, null: false, default: {}
+    create_table :flashes do |t|
+      t.text :session_id, null: false
+      t.text :request_id, null: false
+      t.json :messages,   null: false, default: {}
 
       t.timestamps null: false
     end
+
+    add_index :flashes, [:session_id, :request_id], { unique: true }
   end
 end
