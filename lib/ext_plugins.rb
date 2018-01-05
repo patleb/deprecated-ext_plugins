8d9d1c9e205ce7fe026cb439fbcc497de6828f89
Patrice Lebel <patleb@users.noreply.github.com>
2018-01-05 01:02:39 -0500

    simpler require

--- lib/ext_plugins.rb
+++ lib/ext_plugins.rb
@@ -1,2 +1,23 @@
+require 'ext_async'
+require 'ext_aws_sdk'
+require 'ext_bootstrap'
+# require 'ext_capistrano'
+require 'ext_chartkick'
+require 'ext_coffee'
+require 'ext_devise'
+require 'ext_globals'
+require 'ext_mail'
+require 'ext_pundit'
+require 'ext_rails'
+require 'ext_rails_admin'
+require 'ext_rake'
+require 'ext_settings'
+require 'ext_shell'
+require 'ext_tasks'
+require 'ext_throttler'
+# require 'ext_whenever'
+# require 'sun_cap'
+# require 'sunzistrano'
+
 module ExtPlugins
 end
