21a04e234508bb69b57014c86509582bd27ec9ea
Patrice Lebel <patleb@users.noreply.github.com>
2018-01-07 20:45:00 -0500

    better indexes for mobility migrations

--- ext_rails/db/migrate/20180107145900_create_text_translations.rb
+++ ext_rails/db/migrate/20180107145900_create_text_translations.rb
@@ -5,11 +5,13 @@ class CreateTextTranslations < ActiveRecord::Migration[5.1]
       t.string  :locale
       t.string  :key
       t.text    :value
-      t.integer :translatable_id
-      t.string  :translatable_type
-      t.timestamps
+      t.belongs_to :translatable, polymorphic: true, index: false
+
+      t.timestamps null: false
     end
-    add_index :mobility_text_translations, [:translatable_id, :translatable_type, :locale, :key],
+    add_index :mobility_text_translations, [:translatable_id, :translatable_type, :key, :locale],
       name: :index_mobility_text_translations_on_keys, unique: true
+    add_index :mobility_text_translations, [:value, :translatable_type, :key, :locale],
+      name: :index_mobility_text_translations_on_query_keys
   end
 end
