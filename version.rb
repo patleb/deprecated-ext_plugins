3fe8e9b8df2f29498fcc9f7498dea47a9b4873ae
Patrice Lebel <patleb@users.noreply.github.com>
2018-02-02 15:06:38 -0500

    correction

--- version.rb
+++ version.rb
@@ -1,16 +1,16 @@
-module ExtPlugins
-  def self.gem_version
-    Gem::Version.new VERSION::STRING
-  end
+unless defined? ExtPlugins::STRING
+  module ExtPlugins
+    def self.gem_version
+      Gem::Version.new VERSION::STRING
+    end
 
-  module VERSION
-    unless defined? RAILS_MAJOR
+    module VERSION
       RAILS_MAJOR = 5
       RAILS_MINOR = 1
       MAJOR = 0
       MINOR = 1
-    end
 
-    STRING = [RAILS_MAJOR, RAILS_MINOR, MAJOR, MINOR].compact.join(".")
+      STRING = [RAILS_MAJOR, RAILS_MINOR, MAJOR, MINOR].compact.join(".")
+    end
   end
 end
