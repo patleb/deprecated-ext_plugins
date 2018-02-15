1bd8d9e78a7210dd55510e4555c4d6e677f84345
Patrice Lebel <patleb@users.noreply.github.com>
2018-02-15 06:39:35 -0500

    count_estimate correction

--- ext_rails/lib/ext_rails/active_record/relation.rb
+++ ext_rails/lib/ext_rails/active_record/relation.rb
@@ -1,6 +1,6 @@
 ActiveRecord::Relation.class_eval do
   def count_estimate
-    return 0 if none?
+    return 0 if values[:extending]&.include? ActiveRecord::NullRelation
 
     sql = limit(nil).offset(nil).reorder(nil).to_sql
     connection.execute("EXPLAIN #{sql}").first["QUERY PLAN"].match(/rows=(\d+)/)[1].to_i
