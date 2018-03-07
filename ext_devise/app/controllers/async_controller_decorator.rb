7e8f593dcad11efc44745f6e57156325824af399
Patrice Lebel <patleb@users.noreply.github.com>
2018-03-07 07:54:56 -0500

    orm adapter scope correction

--- ext_devise/app/controllers/async_controller_decorator.rb
+++ ext_devise/app/controllers/async_controller_decorator.rb
@@ -3,7 +3,7 @@ AsyncController.class_eval do
     def set_current
       Current.user ||=
         if (user_id = params[:_user_id])
-          OrmAdapter::ActiveRecord.new(User).get(user_id)
+          ::OrmAdapter::ActiveRecord.new(::User).get(user_id)
         else
           User::Null.new
         end
--- ext_devise/lib/logidze/model_decorator.rb
+++ ext_devise/lib/logidze/model_decorator.rb
@@ -2,7 +2,7 @@ Logidze::Model.module_eval do
   def whodunnit
     if (id = log_data.responsible_id).present?
       # TODO clear on update/undo/redo
-      @_whodunnit ||= OrmAdapter::ActiveRecord.new(User).get(id)
+      @_whodunnit ||= ::OrmAdapter::ActiveRecord.new(::User).get(id)
     end
   end
 end
